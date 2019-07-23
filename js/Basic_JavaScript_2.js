0f5cb673757158969619e425104a0e88e9cf8b95
GavinMunoz <gavin.e.munoz@gmail.com>
2019-07-23 12:59:25 -0500

    Added new HTML and JavaScript files
    Submission in accordance with Dashboard/My Courses/JavScript/What is JavaScript?/Assignment Submission requirements.

---
+++ js/Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function WriteText() {
+    var a = "Hi";
+    var b = "Everyone";
+
+    document.getElementById("test").innerHTML = a + " " + b;
+}
+
+function PlusEquals() {
+    var sentence = " I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("test2").innerHTML = sentence;
+}
