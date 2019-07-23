d94d33d36077319c0608d00479ed41a41d06f169
GavinMunoz <gavin.e.munoz@gmail.com>
2019-07-23 11:48:19 -0500

    Initial Commit
    JavaScript Course

---
+++ js/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+//alert("Hi, I'm an alert.  Clippy is my cousin.");
+//window.alert("Hi, I'm also an alert.  Clippy is my uncle.");
+//document.write("Hi, another alert here.  Clippy is my brother.");
+
+var a = 'She said, "Hi, how are you?"';
+var b = "I told you she said" + ", " + a.substring(a.indexOf('"'));
+document.write(b.fontcolor("red"));
+
+var race = "Saiyans", saiyan_1 = "Goku", saiyan_2 = "Vegeta", saiyan_3 = "Broly";
+document.write(saiyan_2.fontcolor("yellow"));
+
+var blues =" I have the blues";
+blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3+3);
